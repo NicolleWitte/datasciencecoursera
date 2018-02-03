acc85ef6397ea3f623a89e33b325cb4b9b8817e6
NicolleWitte <nicolle.witte@ucdenver.edu>
2018-02-02 17:46:01 -0700

    commiting programming assignment 2.

---
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -0,0 +1,34 @@
+## Programming Assignment 2: Lexical Scoping
+
+
+## makeCacheMatrix creates an object storing the input matrix and its cache.
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+
+## cacheSolve checks if there is a cached inverse matrix and retrieves it if it exists,
+## otherwise it is solved.
+
+cacheSolve <- function(x, ...) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+}
